157fd88b490bea2cc6f73cfa54cf8acb2e3cdeb8
jiahua-ctrl <jiahua@chjh.hc.edu.tw>
2026-08-18 19:27:15 +0800

    接上核心考點去重分析

--- latest_loader_bootstrap.js
+++ latest_loader_bootstrap.js
@@ -3,7 +3,7 @@
   'questions_108_110_backfill_batch21.js','questions_106_107_backfill_batch22.js','questions_107_highschool_depth_batch23.js','questions_107_music_education_batch24.js','questions_108_highschool_depth_batch25.js','questions_111_highschool_depth_batch26.js','questions_112_113_highschool_depth_batch27.js','questions_114_junior_senior_depth_batch28.js','questions_115_junior_senior_depth_batch29.js','questions_115_analysis_depth_batch30.js',
   'terms_108_110_backfill.js','terms_109_highschool_backfill.js','terms_110_highschool_depth.js','terms_111_112_highschool_backfill.js','terms_113_highschool_backfill.js','terms_115_highschool_depth.js','terms_115_moe_changhua_batch31.js',
   'essay_107_lioujia_backfill.js','essay_108_changhua_girls_detailed.js','essay_110_highschool_detailed.js','essay_111_112_highschool_detailed.js','essay_113_highschool_detailed.js','essay_114_highschool_detailed.js','essay_115_junior_senior_detailed.js','essay_115_moe_changhua_batch31.js',
-  'content_adapter_v1.js','radar_106_115_priority_v1.js','question_quality_rules_v1.js','personal_priority.js'
+  'content_adapter_v1.js','radar_106_115_priority_v1.js','question_quality_rules_v1.js','canonical_concepts_v1.js','personal_priority.js'
  ];
- files.forEach(src=>document.write('<script src="'+src+'?v=20260818n"></'+'script>'));
+ files.forEach(src=>document.write('<script src="'+src+'?v=20260818p"></'+'script>'));
 })();
