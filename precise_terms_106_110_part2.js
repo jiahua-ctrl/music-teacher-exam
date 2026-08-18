92bac1363d018c3f41b931b4832ed290c4d6a4ff
jiahua-ctrl <jiahua@chjh.hc.edu.tw>
2026-08-18 14:41:52 +0800

    Expand precise term radar with 107-108 exam sources

---
+++ precise_terms_106_110_part2.js
@@ -0,0 +1,21 @@
+// 精準名詞雷達：106-110 補齊批次 2
+// 原始題源以阿摩「教甄◆音樂專業」實際考卷為準；本檔只做考點索引，不宣稱等於完整逐題轉錄。
+window.PRECISE_TERM_OCCURRENCES = window.PRECISE_TERM_OCCURRENCES || [];
+window.PRECISE_TERM_OCCURRENCES.push(
+  // 107 新北國中
+  ...['Bach《D小調觸技曲與賦格》','管風琴','Beethoven《第九號交響曲》Op.125','Saint-Saëns《動物狂歡節》','Schubert《鱒魚五重奏》D.667','Ravel《Boléro》','Ravel《鵝媽媽》'].map(term=>({term,year:107,level:'國中音樂',source:'107 新北市國中教甄音樂專長',yamol:'https://yamol.tw/exam-107%2B%E5%B9%B4107%E6%96%B0%E5%8C%97%E5%B8%82%E5%9C%8B%E4%B8%AD%E6%95%99%E7%94%84%E9%9F%B3%E6%A8%82%E5%B0%88%E9%95%B774503-74503.htm'})),
+  // 107 臺北國中
+  ...['Mahler《大地之歌》','李白詩歌與音樂','Gregorian chant／格雷果聖歌','Plainsong／素歌','工尺譜','氣鳴樂器','塤','象徵主義','印象主義','移調樂器','French horn／法國號'].map(term=>({term,year:107,level:'國中音樂',source:'107 臺北市國中教師聯合甄選音樂科',yamol:'https://yamol.tw/exam-%E7%84%A1%E5%B9%B4%E5%BA%A6%2B%2B%E5%8F%B0%E5%8C%97%E5%B8%82%E5%9C%8B%E4%B8%AD%E6%95%99%E5%B8%AB%E8%81%AF%E5%90%88%E7%94%84%E9%81%B8%E9%9F%B3%E6%A8%82%E7%A7%9169431-69431.htm'})),
+  // 107 六家高中
+  ...['鄧雨賢','A Cappella／阿卡貝拉','移調樂器','Alto saxophone／中音薩克斯風','原住民族音樂','素養導向教學','議題融入','跨領域課程','多元評量','浪漫樂派','國民樂派','現代樂派','音樂欣賞教學'].map(term=>({term,year:107,level:'高中音樂',source:'107 新竹縣立六家高中音樂科教師甄選',yamol:'https://yamol.tw/exam-107%E5%B9%B4%2B%2B107%2B%E6%96%B0%E7%AB%B9%E7%B8%A3%E7%AB%8B%E5%85%AD%E5%AE%B6%E9%AB%98%E7%B4%9A%E4%B8%AD%E5%AD%B8%E9%9F%B3%E6%A8%82%E7%A7%91%E6%95%99%E5%B8%AB%E7%94%84%E9%81%B896764-96764.htm'})),
+  // 108 新北國中
+  ...['Michala Petri','Recorder／直笛','Dalcroze／達克羅士','Solfège','Eurhythmics／律動','Plastique Animée','A Cappella／阿卡貝拉','呂泉生','南管下四管','響盞'].map(term=>({term,year:108,level:'國中音樂',source:'108 新北市國民中學音樂科',yamol:'https://yamol.tw/exam-108%E5%B9%B4%2B%2B%E6%96%B0%E5%8C%97%E5%B8%82%E5%9C%8B%E6%B0%91%E4%B8%AD%E5%AD%B8%E9%9F%B3%E6%A8%82%E7%A7%9176571-76571.htm'})),
+  // 108 教育部高中聯招
+  ...['Neapolitan sixth／拿坡里六和弦','《黑鬚馬偕》','金希文','Dorian mode／多利安調式','工尺譜','雅樂七聲音階','泛音列','Notre Dame school／聖母院樂派','Ars Antiqua／古藝術','Motet／經文歌'].map(term=>({term,year:108,level:'高中音樂',source:'108 教育部公立高中職教師甄選音樂科',yamol:'https://yamol.tw/exam-108%E5%B9%B4%2B%2B108%E5%B9%B4%E6%95%99%E8%82%B2%E9%83%A8%E5%8F%97%E8%A8%97%E8%BE%A6%E7%90%86%E5%85%AC%E7%AB%8B%E9%AB%98%E4%B8%AD%E8%81%B7%E6%95%99%E7%94%84%E9%9F%B3%E6%A8%82%E7%A7%91%E8%A9%A6%E5%8D%B776083-76083.htm'})),
+  // 108 桃園高中聯招
+  ...['中國五聲音階','中國七聲音階','Mahler《第三號交響曲》','A Cappella／阿卡貝拉','文藝復興音樂','Rock and roll／搖滾樂','Bill Haley','Elvis Presley','The Beatles','Queen'].map(term=>({term,year:108,level:'高中音樂',source:'108 桃園市立高中教師聯合甄選音樂科',yamol:'https://yamol.tw/exam-108%E5%B9%B4%2B%2B108%2B%E6%A1%83%E5%9C%92%E5%B8%82%E7%AB%8B%E9%AB%98%E7%B4%9A%E4%B8%AD%E5%AD%B8_%E6%95%99%E5%B8%AB%E8%81%AF%E5%90%88%E7%94%84%E9%81%B8%E8%A9%A6%E9%A1%8C%EF%BC%9A%E9%9F%B3%E6%A8%82%E7%A7%91124905-124905.htm'})),
+  // 108 明倫高中
+  ...['南管上四管','二絃','三絃','洞簫','琵琶','體鳴樂器','國家文藝獎','客家山歌','大山歌','山歌仔','平板調','Dorian mode／多利安調式','Mass Ordinary／彌撒常套經文','Gloria','Sanctus','Credo','Kyrie','Chaconne／夏康舞曲','Passacaglia／帕薩卡利亞','Dominant seventh chord／屬七和弦'].map(term=>({term,year:108,level:'高中音樂',source:'108 臺北市立明倫高中音樂科教師甄試',yamol:'https://yamol.tw/exam-108%E5%B9%B4%2B%2B%E8%87%BA%E5%8C%97%E5%B8%82%E7%AB%8B%E6%98%8E%E5%80%AB%E9%AB%98%E7%B4%9A%E4%B8%AD%E5%AD%B8%E9%9F%B3%E6%A8%82%E7%A7%91%E6%95%99%E5%B8%AB%E7%94%84%E8%A9%A677312-77312.htm'})),
+  // 108 高師大附中
+  ...['West Side Story／西城故事','Noh／能劇','地謠 jiutai','陳錫煌','Schubert《Gretchen am Spinnrade》','Modified strophic song／變化分節歌','Mesto','Arioso','Calando','Grave','Queen《Bohemian Rhapsody》'].map(term=>({term,year:108,level:'高中音樂',source:'108 高師大附中高中音樂科教師甄試',yamol:'https://yamol.tw/exam-108%E5%B9%B4%2B%2B%E9%AB%98%E5%B8%AB%E5%A4%A7%E9%99%84%E4%B8%AD%2B108%2B%E5%AD%B8%E5%B9%B4%E5%BA%A6%E9%AB%98%E4%B8%AD%E9%9F%B3%E6%A8%82%E7%A7%91%E6%95%99%E5%B8%AB%E7%94%84%E8%A9%A6%E8%A9%A6%E9%A1%8C76047-76047.htm'}))
+);
