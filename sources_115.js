0230733ef48ffb434d2e0fdde0cca38786f359d7
jiahua-ctrl <jiahua@chjh.hc.edu.tw>
2026-08-18 18:26:09 +0800

    Load second 114 Central Taiwan music question and terminology batches

--- sources_115.js
+++ sources_115.js
@@ -4,11 +4,13 @@ document.write('<script src="questions_114_kshs_batch22.js?v=20260818"><\/script
 document.write('<script src="questions_114_newtaipei_jhs_batch23.js?v=20260818"><\/script>');
 document.write('<script src="questions_114_newtaipei_batch24.js?v=20260818"><\/script>');
 document.write('<script src="questions_114_central_jhs_batch25.js?v=20260818"><\/script>');
+document.write('<script src="questions_114_central_jhs_batch26.js?v=20260818"><\/script>');
 document.write('<script src="terms_114_moe_batch22.js?v=20260818"><\/script>');
 document.write('<script src="terms_114_kshs_batch23.js?v=20260818"><\/script>');
 document.write('<script src="terms_114_hsnu_batch25.js?v=20260818"><\/script>');
 document.write('<script src="terms_114_klsh_batch26.js?v=20260818"><\/script>');
 document.write('<script src="terms_114_central_jhs_batch27.js?v=20260818"><\/script>');
+document.write('<script src="terms_114_central_jhs_batch28.js?v=20260818"><\/script>');
 document.write('<script src="essay_114_moe_batch.js?v=20260818"><\/script>');
 document.write('<script src="essay_114_kshs_batch.js?v=20260818"><\/script>');
 document.write('<script src="essay_114_hchs_batch.js?v=20260818"><\/script>');
