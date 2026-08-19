ad9be0938a17ab4a67c4728a8545fbc8a7c0afe2
jiahua-ctrl <jiahua@chjh.hc.edu.tw>
2026-08-19 22:36:37 +0800

    Load bounded daily term plan

--- detailed_answer_ui.js
+++ detailed_answer_ui.js
@@ -24,6 +24,7 @@
     loadScript('term-points-mode.js?v=20260819a','term-points-mode');
     loadScript('term-next-action.js?v=20260819a','term-next-action');
     loadScript('term-readiness-summary.js?v=20260819b','term-readiness-summary');
+    loadScript('term-daily-plan.js?v=20260819a','term-daily-plan');
   }
   loadBootTools();
   function mount(){
