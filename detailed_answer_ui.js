47efc891c416ba369434f2c043074b3d89a4031c
jiahua-ctrl <jiahua@chjh.hc.edu.tw>
2026-08-19 19:29:13 +0800

    Load trial teaching diagnosis

--- detailed_answer_ui.js
+++ detailed_answer_ui.js
@@ -8,7 +8,8 @@
     loadScript('term-learning.js?v=20260818d','term-learning');
     loadScript('bad-question.js?v=20260818a','bad-question');
     loadScript('nonchoice-center.js?v=20260819a','nonchoice-center');
-    loadScript('nonchoice-progress.js?v=20260819a','nonchoice-progress');
+    loadScript('nonchoice-progress.js?v=20260819b','nonchoice-progress');
+    loadScript('trial-diagnosis.js?v=20260819a','trial-diagnosis');
   }
   loadBootTools();
   function mount(){
